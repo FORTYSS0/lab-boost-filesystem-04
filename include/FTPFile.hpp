565fec4e69339e6c0500621b2c1cd567b200762b
FORTYSS <dmitriy_sviridov_2001@mail.ru>
2021-01-24 06:13:32 +0300

    finish

--- include/FTPFile.hpp
+++ include/FTPFile.hpp
@@ -55,7 +55,7 @@ class FTPFile {
   void Check(const path& pat);
   void Unique(std::vector<string>);
   std::vector<string> AcName();
-  friend std::ostream& operator << (std::ostream&, const FTPFile& scanner);
+  friend std::ostream& operator << (std::ostream&, FTPFile&);
   void OutAll (std::ostream&);
   void OutUnique(std::ostream&);
  protected:
